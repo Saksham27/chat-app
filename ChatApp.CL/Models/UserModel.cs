8100938335263226993be8ef996db5e785bd8fee
Saksham27 <geekysaksham27@gmail.com>
2020-11-20 13:48:01 +0530

    Added : POCO class for User

---
+++ ChatApp.CL/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ChatApp.CL.Models
+{
+    [DataContract]
+    public class UserModel
+    {
+        [DataMember(Name = "Email")]
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression("^[a-zA-Z0-9]{1,}([.]?[-]?[+]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2})?$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+
+        [DataMember(Name = "Password")]
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression("^.{8,15}$", ErrorMessage = "Password Length should be between 8 to 15")]
+        public string Password { get; set; }
+
+        [DataMember(Name = "UserName")]
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
